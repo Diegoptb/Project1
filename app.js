57f2fe3ea516841e3b9d68432195eec4c92aebf9
Diego <122508333+Diegoptb@users.noreply.github.com>
2024-12-19 03:40:07 -0600

    Producto final (?
    Todas las correcciones agregadas

--- app.js
+++ app.js
@@ -1,39 +1,61 @@
+document.addEventListener('DOMContentLoaded', function() {
+
 //Imprime un mensaje de bienvenida en la consola
 console.log("Sistema EBAC de registro de alumnos");
+
+var form = document.getElementById('registration-form');
+var addButton = document.getElementById('add-button');
+var consultButton = document.getElementById('consult-button');
+var deleteButton = document.getElementById('delete-button');
+
+form.addEventListener('submit',agregarAlumno);
+
+form.style.display = 'none';
+
+addButton.addEventListener('click',function(event){
+    form.style.display = 'flex';
+});
+consultButton.addEventListener('click',consultarRegistros);
+deleteButton.addEventListener('click',eliminarAlumno);
+
 
 
 //Se define un arreglo de registro con datos de alumnos en EBAC
 var registro=[
-    {nombre: "Salvador Mora", edad: "30", zonaResidencia: "CDMX", nombrePrograma: "Desarrollador Front End De Cero a Pro", email: "salvadormora@ebac.mx", seguroSocial: "Sí"},
-    {nombre: "Maria Hubert", edad: "34", zonaResidencia: "Edo Mex", nombrePrograma: "Desarollo Back End con Python", email: "maria@ebac.mx", seguroSocial: "Sí"},
-    {nombre: "Juan López", edad: "28", zonaResidencia: "GDL", nombrePrograma: "Data Science Avanzado", email: "juanlopez@ebac.mx", seguroSocial: "No"},
-    {nombre: "Alejandra Ramirez", edad: "32", zonaResidencia: "MTY", nombrePrograma: "Marketing Digital", email: "alejandramirez@ebac.mx", seguroSocial: "No"},
-    {nombre: "Carlos Rodríguez", edad: "29", zonaResidencia: "CDMX", nombrePrograma: "UX/UI Design", email: "carlosrodriguez@ebac.mx", seguroSocial: "Sí"}
+    {nombre: "Salvador Mora", edad: "30", zonaResidencia: "CDMX", nombrePrograma: "Desarrollador Front End De Cero a Pro", email: "salvadormora@ebac.mx"},
+    {nombre: "Maria Hubert", edad: "34", zonaResidencia: "Edo Mex", nombrePrograma: "Desarollo Back End con Python", email: "maria@ebac.mx"},
+    {nombre: "Juan López", edad: "28", zonaResidencia: "GDL", nombrePrograma: "Data Science Avanzado", email: "juanlopez@ebac.mx"},
+    {nombre: "Alejandra Ramirez", edad: "32", zonaResidencia: "MTY", nombrePrograma: "Marketing Digital", email: "alejandramirez@ebac.mx"},
+    {nombre: "Carlos Rodríguez", edad: "29", zonaResidencia: "CDMX", nombrePrograma: "UX/UI Design", email: "carlosrodriguez@ebac.mx"}
 ];
 
 //Función para agregar un nuevo alumno al registro
-function agregarAlumno(){
+function agregarAlumno(event){
 
+    event.preventDefault();
     //Se solicita al usuario ingresar los datos del nuevo alumno
-    var nombre = prompt("Ingresa el nombre de alumno");
-    var zonaResidencia = prompt("Ingresa la zona de residencia del alumno")
-    var edad = prompt("Ingresa la edad del alumno")
-    var nombrePrograma = prompt("Ingresa el programa a estudiar")
-    var email = prompt("Ingresa el correo electrónico")
-    var seguroSocial = prompt("¿Cuenta con seguro social? (Sí/No)");
+    var nombre = document.getElementById('name-input').value;
+    var zonaResidencia = document.getElementById('locality-input').value;
+    var edad = document.getElementById('age-input').value;
+    var nombrePrograma = document.getElementById('course-input').value;
+    var email = document.getElementById('email-input').value;
 
-    //Se crea un objeto con los datos del nuevo alumno
-    var nuevoAlumno = {nombre: nombre, edad: edad, zonaResidencia: zonaResidencia, nombrePrograma: nombrePrograma, email: email, seguroSocial: seguroSocial};
-    //Se agrega el nuevo alumno al arreglo del registro
+    var nuevoAlumno = {nombre: nombre, edad: edad, zonaResidencia: zonaResidencia, nombrePrograma: nombrePrograma, email: email};
+
     registro.push(nuevoAlumno);
-    alert("Alumno agregado correctamente");
+
+    var nombre = document.getElementById('name-input').value = '';
+    var zonaResidencia = document.getElementById('locality-input').value = '';
+    var edad = document.getElementById('age-input').value = '';
+    var nombrePrograma = document.getElementById('course-input').value = '';
+    var email = document.getElementById('email-input').value = '';
+    form.style.display = 'none';
+
 }
 
-
-//Función para consultar los registros de los alumnos
 function consultarRegistros(){
     //Este ciclo itera sobre cada registro del arreglo
-    for(var i = 0; i < registro.length; i++){
+    /*for(var i = 0; i < registro.length; i++){
         //Esta lista permite ver al usuario los datos del registro en la consola
         console.log("Nombre: " + registro[i].nombre);
         console.log("Edad: " + registro[i].edad);
@@ -47,7 +69,8 @@ function consultarRegistros(){
         }
         console.log("Cuenta con seguro social: " + registro[i].seguroSocial);
         console.log("_________________________");
-    };
+    };*/
+    console.table(registro);
 }
 
 
@@ -62,7 +85,7 @@ function eliminarAlumno(){
 }
 
 
-//Permite repetir el menú de opciones hasta que el usuario decida salir
+/*
 do{
 
 //Variable que toma la elección del usuario para ejecutar la función indicada
@@ -86,6 +109,6 @@ if (opcion === "1") {
     var continuar = prompt("¿Desea realizar otra operación? (S/N)");
     //cierre del ciclo
 } while(continuar === "s");
+*/
 
-
-
+});
